08e236513d089c97c637f2e9d65cf343c96d026f
Madison Krell <55954812+makrell38@users.noreply.github.com>
2022-04-02 11:34:01 -0400

    Add in community detection algorithm

--- sequential/sinWDPVGExample.jl
+++ sequential/sinWDPVGExample.jl
@@ -3,6 +3,7 @@ using JuMP
 using Distributions
 
 include("WDPVG.jl")
+include("communityDetection.jl")
 
 #set number of points
 numPoints = 10
@@ -10,7 +11,15 @@ numPoints = 10
 x = rand(Uniform(0,1),1,numPoints)
 #s holds output of each x value put into the sin function
 s = sin.(x)
-#call WDPVG which returns the WDPVG graph of numPoints x numPoints
+#call WDPVG which returns the WDPVG list of tuples of edges
 WDPVG = build_WDPVG(s, numPoints)
-display("text/plain", WDPVG)
-println()
+
+println(WDPVG)
+#create graph using method from communityDetection.jl
+graph = Digraph(WDPVG)
+
+source = 1
+destination = numPoints
+
+path, cost = dijkstrapath(graph, source, destination)
+println("Shortest path from $source to $destination: ", isempty(path) ? "no possible path" : join(path, " → "), " (cost $cost)")
